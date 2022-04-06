8704a2beeada20d75b7ea7b31e4622107b2baccc
irva123 <irvaputrifinisha2@gmail.com>
2022-04-06 07:35:56 +0700

    menambahkan sign up

--- lib/pages/first_signup.dart
+++ lib/pages/first_signup.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:smart_umkm/pages/first_signup.dart';
+import 'package:smart_umkm/pages/first_signin.dart';
 
-class SignIn extends StatelessWidget {
-  static String tag = 'Sign-in';
+class SignUp extends StatelessWidget {
+  static String tag = 'sign-up';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,14 +13,15 @@ class SignIn extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(
-              'assets/logokecil.png',
+            Center(
+              child: Image.asset('assets/logokecil.png',
               width: 106,
               height: 100,
+            )
             ),
             SizedBox(height: 50),
             Text(
-              'Sign In Account \nWelcome back',
+              'Sign Up Account',
               style: GoogleFonts.poppins(
                 color: Colors.white,
                 fontSize: 22,
@@ -48,7 +49,34 @@ class SignIn extends StatelessWidget {
                   borderRadius: BorderRadius.circular(15),
                   borderSide: BorderSide.none,
                 ),
-                hintText: 'Eva Monika Septiana',
+                hintText: 'Username',
+                hintStyle: GoogleFonts.poppins(
+                  color: Color(0xffAAAAAA),
+                ),
+              ),
+            ),
+             SizedBox(height: 10),
+            Text(
+              'Email',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xff0D134C),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Email',
                 hintStyle: GoogleFonts.poppins(
                   color: Color(0xffAAAAAA),
                 ),
@@ -79,23 +107,41 @@ class SignIn extends StatelessWidget {
                 hintText: 'Password',
                 hintStyle: GoogleFonts.poppins(
                   color: Color(0xffAAAAAA),
-                ),
-                suffixIcon: Icon(
-                  Icons.visibility,
-                  color: Color(0xff919191),
-                ),
+                ),               
               ),
             ),
             SizedBox(height: 6),
-            Container(
-              alignment: Alignment(1, 0.5),
-              child: Text(
-                'Forget My Password',
-                style: GoogleFonts.poppins(
-                  color: Color(0xffF2F2F2),
-                ),
+
+                    SizedBox(height: 16),
+            Text(
+              'Confirm Password',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
               ),
             ),
+            SizedBox(height: 8),
+            TextFormField(
+              obscureText: true,
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xff0D134C),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Confirm Password',
+                hintStyle: GoogleFonts.poppins(
+                  color: Color(0xffAAAAAA),
+                ),               
+              ),
+            ),
+            SizedBox(height: 6),
+          
             SizedBox(height: 25),
             Container(
               width: 365,
@@ -107,9 +153,11 @@ class SignIn extends StatelessWidget {
                   ),
                   backgroundColor: Color(0xffFCAC15),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                Navigator.of(context).pushNamed(SignIn.tag);
+              },
                 child: Text(
-                  'Sign In',
+                  'Sign Up',
                   style: GoogleFonts.poppins(
                     color: Color(0xffFFFFFF),
                     fontSize: 16,
@@ -117,44 +165,7 @@ class SignIn extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20, left: 70),
-              child: Row(
-                children: [
-                  Text(
-                    "Dont have account? ",
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      //alignment: Alignment(1, 0.5),
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Container(
-                  child: TextButton(
-                  style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  backgroundColor: Color(0xffFCAC15),
-                ),
-                  onPressed: () {
-                  Navigator.of(context).pushNamed(SignUp.tag);
-                 },
-                  child:Text(
-                    'Sign Up',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      decoration: TextDecoration.underline,              
-                    ),               
-                  )
-                  )
-                  )
-                ],
-                  
-              ),
-            ),
+            ),           
           ],
         ),
       ),
